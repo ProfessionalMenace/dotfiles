c1037fa5ab60a9f07a3fb9ac3d8a8e25a2daac6f
ProfessionalMenace <vondracekadam00@gmail.com>
2025-03-16 20:41:13 +0100

    Updated all
    moved .config -> config
    added river config
    updated nvim description
    added waybar config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,9 +1,7 @@
--- Geometry
+-- This is my minimal configuration
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.wrap = false
--- Colorscheme
-vim.cmd.colorscheme("catppuccin-mocha")
